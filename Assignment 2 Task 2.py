1e75e40b2d1467d06876fb1efb825d605960198c
Sanketkate385 <sanket.kate385@gmail.com>
2025-06-09 22:24:17 +0530

    Assignment 2 Task 2.py

---
+++ Assignment 2 Task 2.py
@@ -0,0 +1,9 @@
+# Task 2:
+    
+
+Num1 = int(input("Enter the First number: "))
+Num2 = int(input("Enter the Second number: "))
+total = 0
+for i in range (Num1, Num2 + 1):
+    total += i
+print("Total Sum of",Num1,"and",Num2,"is:",total)
